822c9c9d1aeb903b61da2367702b77bc3d768cb6
gmsz46u-byte <gmsz46u@gmail.com>
2025-12-30 09:10:50 +0200

    remove config.py

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-import os
 
-class config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///Urology_app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    CKEDITOR_ENABLE_CODESNIPPET = True
-    CKEDITOR_FILE_UPLOADER = "main.upload"
-    MAIL_SERVER = "smtp.googlemail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("EMAIL_USER")
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
